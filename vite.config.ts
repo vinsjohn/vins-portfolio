9be871d0db663e9202429a4f5be4d73c1838566a
vinsjohn <vinsjohn2016@gmail.com>
2026-08-16 05:52:53 +0400

    Remove kimi plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import { inspectAttr } from 'kimi-plugin-inspect-react'
+
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [inspectAttr(), tailwindcss(), react()],
+  plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
